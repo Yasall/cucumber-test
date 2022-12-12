d3fbabbdf00f72e5d1caaafb9699e59fd42f6017
Yasin <yasinallie@gmail.com>
2022-12-12 06:53:03 +0200

    cucumber-practice

---
+++ cypress.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require("cypress");
+const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
+const {createEsbuildPlugin} = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: 'cypress/e2e/Features/*.feature',
+    chromeWebSecurity: false,
+    baseUrl: 'https://www.saucedemo.com/',
+    async setupNodeEvents(on, config) {
+      
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+            plugins: [createEsbuildPlugin(config)],
+          })
+        );
+        return config;
+    
+      
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress/e2e/Features/Login.feature
@@ -0,0 +1,10 @@
+Feature: Logging in a website
+
+    Logging in to sauce demo website
+    
+ Scenario: Successfull login in to website
+ Given
+ Then  I enter the username "standard_sauce"
+ Then A user enters the password "secret_sauce"
+ Then A user clicks on the login button
+ 
